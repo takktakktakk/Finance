3cf6c2a66775422122f840741b10e87666f52cab
{takktakktakk} <{takk.ohno520@gmail.com}>
2026-02-23 09:56:10 +0900

    コーヒープロジェクトのモデルパッケージと静的画像を追加し、`tmp.py`をmatplotlibのプロット表示に更新しました。

--- Nikkei/nikkei_scraping.py
+++ Nikkei/nikkei_scraping.py
@@ -1,14 +1,10 @@
-import datetime
 import requests
 import json
 import pandas as pd
 
-# 現在時刻取得
-now_date = datetime.datetime.now()
-
-# GMOコインのAPIで最新の1分足の為替レートを取得
+# GMOコインのAPIで最新の為替レートを取得
 end_point = 'https://forex-api.coin.z.com/public'
-api_path = f'/v1/klines?symbol=USD_JPY&priceType=ASK&interval=1hour&date={now_date.strftime("%Y%m%d")}'
+api_path = f'/v1/ticker'
 
 response = requests.get(f'{end_point}{api_path}')
 if response.status_code != 200:
@@ -22,3 +18,7 @@ print(data)
 # pandasにデータを格納
 df = pd.DataFrame(data["data"])
 print(df)
+
+# USDJPYのデータのみ抽出
+usd_jpy_data = df.loc[df['symbol'] == 'USD_JPY']
+print(usd_jpy_data)
